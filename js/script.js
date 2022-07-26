16b903f27a3d680308fd6e657a29d34adaada102
ibrohim17 <ibragimkarimjonov@gmail.com>
2022-07-26 14:45:31 +0500

    js

---
+++ js/script.js
@@ -0,0 +1,15 @@
+var intro = alert("Welcome to our site!");
+
+var first_name = prompt("What is your name?", "Ibrohim");
+
+var last_name = prompt("What is your last name?", "Karimjonov");
+
+var how_old = prompt("How old are you?", "17");
+
+var profession = prompt("What is your profession?", "Progromist");
+
+var home = prompt("Where do you live?", "Toshkent");
+
+var confirm_info = confirm("Thank you :)");
+
+console.log(`informatcia: ${first_name} ${last_name} ${how_old} ${profesion} ${home} ${confirm_info}`);
